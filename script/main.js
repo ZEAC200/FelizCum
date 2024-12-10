460f942e790c8b57b1e1967f8867f2dc9994233d
ZEAC200 <149036757+ZEAC200@users.noreply.github.com>
2024-12-10 16:31:58 -0600

    Update main.js

--- script/main.js
+++ script/main.js
@@ -1,7 +1,7 @@
 // trigger to play music in the background with sweetalert
 window.addEventListener('load', () => {
     Swal.fire({
-        title: '¿Quiers reproducir la música del fondo?',
+        title: '¿Quieres reproducir la música del fondo?',
         icon: 'warning',
         showCancelButton: true,
         confirmButtonColor: '#3085d6',

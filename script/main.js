1c0096707281238041d6bfe66753d4552b97c81a
ZEAC200 <149036757+ZEAC200@users.noreply.github.com>
2024-12-11 23:42:25 -0600

    Update main.js

--- script/main.js
+++ script/main.js
@@ -85,7 +85,7 @@ const animationTimeline = () => {
             opacity: 0,
             y: 10
         },
-    "+=3")
+    "+=10000")
     .from(".four", 0.7, {
         scale: 0.2,
         opacity: 0,
